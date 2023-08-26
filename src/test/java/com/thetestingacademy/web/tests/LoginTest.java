14e320c57219a7cef9a35f21ed8a10233bb1f340
ATTAQA <pramoddutta_96@yahoo.com>
2023-08-26 06:57:12 +0530

    26 Aug 2023

--- src/test/java/com/thetestingacademy/web/tests/LoginTest.java
+++ src/test/java/com/thetestingacademy/web/tests/LoginTest.java
@@ -1,48 +1,63 @@
 package com.thetestingacademy.web.tests;
 
+import com.aventstack.extentreports.ExtentTest;
 import com.thetestingacademy.driver.DriverManagerTL;
-import com.thetestingacademy.pages.DashboardPage;
 import com.thetestingacademy.pages.LoginPage;
 import com.thetestingacademy.utils.ProperReader;
-import com.thetestingacademy.utils.ScreenShotListener;
 import com.thetestingacademy.web.testbase.TestBase;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
-import org.testng.annotations.Listeners;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 import org.assertj.core.api.Assertions;
 
-@Listeners(ScreenShotListener.class)
+import java.lang.reflect.Method;
+
+
 public class LoginTest extends TestBase { // Inheritance
+    ExtentTest test;
+    private static final Logger logger = LogManager.getLogger(LoginTest.class);
 
 
     @Severity(SeverityLevel.CRITICAL)
     @Description("Verify the Valid Credentials are working file")
-    @Test(groups = {"P0", "negative"},priority = 1)
-    public void testLoginNegative() throws Exception {
+//    @Test(groups = {"P0", "negative", }, priority = 1)
+    @Test(groups = {"tagName", "t:another-tagName", "a:authorName", "d:deviceName"})
+    public void invalid_loginVWO(Method method) throws Exception {
         // Navigate, Login to VWO and Assert
         // Abstraction
+        test = createTest(method.getName());
         DriverManagerTL.getDriver().get(ProperReader.readKey("url"));
         String expectResult = new LoginPage().loginToVWO(false).error_message_text();
+        logger.info("Failed");
+
+        if (expectResult.equalsIgnoreCase(expectResult)) {
+            logger.info("Failed");
+            test.fail("Failed Testcases");
+            test.addScreenCaptureFromBase64String(captureScreenshot(DriverManagerTL.getDriver()));
+            takeScreenShot(method.getName(),DriverManagerTL.getDriver());
+        }
+
         Assertions.assertThat(expectResult)
                 .isNotBlank().isNotNull().contains(ProperReader.readKey("expected_error"));
 
     }
-
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Verify the Invalid Credentials are working file")
-    @Test(groups = {"P0", "positive"},priority = 2)
-    public void testLoginPositive() throws Exception {
-        // Navigate, Login to VWO and Assert
-        // Abstraction
-        DriverManagerTL.getDriver().get(ProperReader.readKey("url"));
-        DashboardPage dashboardPage = new LoginPage().loginToVWO(true).afterLogin();
-        String expectResult = dashboardPage.loggedInUserName();
-        Assertions.assertThat(expectResult)
-                .isNotBlank().isNotNull().contains(ProperReader.readKey("expected_username"));
-
-    }
+//
+//    @Severity(SeverityLevel.CRITICAL)
+//    @Description("Verify the Invalid Credentials are working file")
+//    @Test(groups = {"P0", "positive"},priority = 2)
+//    public void testLoginPositive() throws Exception {
+//        // Navigate, Login to VWO and Assert
+//        // Abstraction
+//        DriverManagerTL.getDriver().get(ProperReader.readKey("url"));
+//        DashboardPage dashboardPage = new LoginPage().loginToVWO(true).afterLogin();
+//        String expectResult = dashboardPage.loggedInUserName();
+//        Assertions.assertThat(expectResult)
+//                .isNotBlank().isNotNull().contains(ProperReader.readKey("expected_username"));
+//
+//    }
 
 
 }
