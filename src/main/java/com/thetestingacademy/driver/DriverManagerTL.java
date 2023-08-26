605af05c8888d3776486608c0b92a380a505f9b2
ATTAQA <pramoddutta_96@yahoo.com>
2023-08-26 08:36:51 +0530

    26 Aug 2023 fix3

--- src/main/java/com/thetestingacademy/driver/DriverManagerTL.java
+++ src/main/java/com/thetestingacademy/driver/DriverManagerTL.java
@@ -4,8 +4,15 @@ import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DriverManagerTL {
     // Thread Local - Java
 
@@ -25,12 +32,33 @@ public class DriverManagerTL {
     }
 
     @BeforeMethod
-    public static void init(){
+    public static void init() throws MalformedURLException {
         if(getDriver() == null){
             // TODO #1 - Need to make it support for Firefox, Edge, Safari, and IE
+
             ChromeOptions options = new ChromeOptions();
-            options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-            setDriver(new ChromeDriver(options));
+            options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+                /* How to add test badge */
+                put("name", "Test badge...");
+
+                /* How to set session timeout */
+                put("sessionTimeout", "15m");
+
+                /* How to set timezone */
+                put("env", new ArrayList<String>() {{
+                    add("TZ=UTC");
+                }});
+
+                /* How to add "trash" button */
+                put("labels", new HashMap<String, Object>() {{
+                    put("manual", "true");
+                }});
+
+                /* How to enable video recording */
+                put("enableVideo", true);
+            }});
+            driver = new RemoteWebDriver(new URL("http://52.23.178.106:4444/wd/hub"), options);
+            setDriver(driver);
         }
     }
 
